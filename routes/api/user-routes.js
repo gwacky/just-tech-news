033bdb84130be1eab62102ecf8fe877bd73c920f
Gwack <gracalynntrammell@gmail.com>
2021-12-06 22:00:53 -0600

    used hooks to hash the password

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -51,11 +51,16 @@ router.put('/:id', (req, res) => {
   // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
 
   // pass in req.body instead to only update what's passed through
-  User.update(req.body, {
-    where: {
-      id: req.params.id
-    }
-  })
+  router.put('/:id', (req, res) => {
+    // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+  
+    // pass in req.body instead to only update what's passed through
+    User.update(req.body, {
+      individualHooks: true,
+      where: {
+        id: req.params.id
+      }
+    })
     .then(dbUserData => {
       if (!dbUserData[0]) {
         res.status(404).json({ message: 'No user found with this id' });
